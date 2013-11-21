92a78dddd7701535c62ca53e289f953daa7fed95
Gabriel Rodríguez Alberich <chewie@gmail.com>
2013-11-21 12:46:41 +0100

    Removed dark overlay that was breaking Firefox.

--- jekyll_site/assets/js/scripts.js
+++ jekyll_site/assets/js/scripts.js
@@ -20,25 +20,9 @@ $(document).ready(function() {
 $(document).ready(function() {
     var vpH = $(window).height();
     var vH = vpH - 350;
-    $('.overlay').css("height", vH);
     $('.featured-image').css("height", vH);
     $('.image-credit').css("height", vH);
     $('.image-credit-container').css("height", vH);
-});
-
-
-$(function(){
-  $('<img>').attr('src',function(){
-      var imgUrl = $('div.featured-image').css('background-image');
-      if (!imgUrl) {
-        return;
-      }
-      imgUrl = imgUrl.substring(4, imgUrl.length-1);
-      return imgUrl;
-  }).load(function(){
-    $('img.loading').fadeOut(500);
-    $('div.overlay').fadeTo("slow", 0.0);
-  });
 });
 
 $(function(){
